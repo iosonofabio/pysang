50196de926cf21f93dd04e261cf5600daedb396e
Fabio Zanini <fabio.zanini@tuebingen.mpg.de>
2013-12-15 17:08:34 +0100

    [BUGFIX] Nesting imports in gui.py

--- pysang/gui.py
+++ pysang/gui.py
@@ -10,17 +10,16 @@ def main():
     try:
         import PySide
         from gui_pyside import main as main_gui
-        main_gui()
 
     except ImportError:
         try:
             import Tkinter
             from gui_tk import main as main_gui
-            main_gui()
 
         except ImportError:
             raise ImportError('Neither PySide nor Tk libraries found')
 
+    main_gui()
 
 
 # Script
