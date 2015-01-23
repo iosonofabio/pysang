ba304c00a07b536b9557eb9f8d1f31880bedd98d
Fabio Zanini <fabio.zanini@tuebingen.mpg.de>
2015-01-23 02:06:59 +0100

    Try to install from Pypi

--- setup.py
+++ setup.py
@@ -7,19 +7,17 @@ content:    Setup file for PySang
 import os
 from setuptools import setup, find_packages
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+long_description = "Check out the README file for install instructions and more."
 
 setup(name="PySang",
       version="0.3.6",
       description="Visualizer for Sanger chromatographs (ABI/AB1).",
-      long_description=read("README.md"),
+      long_description=long_description,
       author="Fabio Zanini",
       author_email="fabio.zanini@tuebingen.mpg.de",
       license='Public domain',
       url='https://github.com/iosonofabio/pysang',
       packages=find_packages(exclude='tests'),
       package_data={'pysang': ['data/*.ab1']},
-      install_requires=['matplotlib', 'biopython'],
       entry_points = {'gui_scripts': ['pysang = pysang.gui:main']}
      )
